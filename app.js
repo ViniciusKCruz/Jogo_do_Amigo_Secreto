31bcb97505db26eb45e69e49af2349265220d09e
ViniciusKCruz <vinicius.ksantos01@gmail.com>
2025-03-10 21:53:24 -0300

    Funcao adicionarAmigo() melhorada e agora funcional e também implementacao da funcao atualizarLista()

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
 let listaDeAmigos = [];
+let nomeAmigo;
 
-function adicionarAmigo(){
-    let nomeAmigo = document.getElementById('amigo').value;
-    listaDeAmigos.push(nomeAmigo);
+function adicionarAmigo() {
+    let inserirAmigo = document.getElementById('amigo'); 
+    let nomeAmigo = inserirAmigo.value.trim(); 
+
+    if (nomeAmigo === '') {
+        alert('Por favor, insira um nome válido!');
+        return;
+    }
+
+    listaDeAmigos.push(nomeAmigo); 
+    inserirAmigo.value = ''; 
+
+    atualizarLista();
 }
 
+function atualizarLista(){
+    let atualizandoLista = document.getElementById('listaAmigos');
+    atualizandoLista.innerHTML = listaDeAmigos.map(amigo => `<li>${amigo}</li>`).join('');
+}
